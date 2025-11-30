08e60dc11b005f072d9451d09275ef9818e120b4
Aayush S <Shah18940@gmail.com>
2025-11-29 22:39:30 -0500

    Finished implementation of the PlaylistRecommender class

--- src/main/java/edu/pitt/se/PlaylistRecommender.java
+++ src/main/java/edu/pitt/se/PlaylistRecommender.java
@@ -6,13 +6,14 @@ public class PlaylistRecommender {
 
     public static String classifyEnergy(List<Integer> bpms) {
 
-        // if nothing is contained within the list
-        if (bpms.size() == 0) {
-            return "Invalid Size, List must contain at least one item";
-        }
         // if the list is null invalid
         if (bpms == null) {
             return "Invalid, List is null";
+        }
+
+        // if nothing is contained within the list
+        if (bpms.size() == 0) {
+            return "Invalid Size, List must contain at least one item";
         }
 
         int sum = 0;
@@ -36,23 +37,33 @@ public class PlaylistRecommender {
         }
     }
 
-    //Checks for alphabetic characters + spaces, 1–30 chars
-    //Reject null or special characters   
     public static boolean isValidTrackTitle(String title) {
-        // if the title it less than 1 char or greater than 30, invalid string
-        if(title.length() < 1 || title.length() > 30) {
+        // if the title is null, less than 1 char, or greater than 30 chars in length
+        // it is an invalid title
+        if (title == null || title.length() < 1 || title.length() > 30) {
             return false;
         }
-        
-        return title.matches("[a-zA-Z]");
+
+        // for each character in the string if it is not a letter or a space,
+        // it is not valid, and the track title is not valid as well
+        for (int i = 0; i < title.length(); i++) {
+
+            char curr = title.charAt(i);
+
+            if (!Character.isLetter(curr) || curr != ' ') {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static int normalizeVolume(int volumeDb) {
-        if(volumeDb > 100){
+        // Clamp volume into range 0–100
+        if (volumeDb > 100) {
             return 100;
-        }else if(volumeDb < 0){
+        } else if (volumeDb < 0) {
             return 0;
-        }else{
+        } else {
             return volumeDb;
         }
     }
